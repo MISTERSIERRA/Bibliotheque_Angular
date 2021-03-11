6732bb509aa7b9790d4bd4f0666b09869a6e7180
MISTERSIERRA <77048872+MISTERSIERRA@users.noreply.github.com>
2021-03-11 08:13:18 +0100

    restore

--- src/app/bande-dessine/bande-dessine.component.ts
+++ src/app/bande-dessine/bande-dessine.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DonneesService } from '../services/donnees-services';
 
 @Component({
   selector: 'app-bande-dessine',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BandeDessineComponent implements OnInit {
 
-  constructor() { }
+  constructor(private donneesService: DonneesService) { }
+
+  recuplivreNumeroGuide = "nombreGuide";
+  recuplivreTitre = "inconnu";
+  recuplivreSerie = "inconnu";
+  recuplivreAuteur = "inconnu";
+  recuplivreNumeroVolume = "inconnu";
+  recuplivreSRC = "inconnu";
+
 
   ngOnInit(): void {
   }
