ccf7d5523af408fac0b4e8292fa8471ac1d82909
MISTERSIERRA <77048872+MISTERSIERRA@users.noreply.github.com>
2021-03-15 16:05:51 +0100

    maj

--- src/app/barre-recherche/barre-recherche.component.ts
+++ src/app/barre-recherche/barre-recherche.component.ts
@@ -43,9 +43,7 @@ export class BarreRechercheComponent implements OnInit {
   onButtonSubmitted() {
     console.log("appui bouton");
     /***********************************************************/
-
-    //this.name = .......input.....
-
+    this.name = (<HTMLInputElement>document.getElementById('recupValeurBtn')).value;
     /***********************************************************/
     this.donneesServices.input_recherche = this.name;
     console.log("fonction 3 : " + this.donneesServices.input_recherche);
